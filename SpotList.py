399cbdc89d714a553470810c47accc34a169dc12
Greguru0 <41220649+Greguru0@users.noreply.github.com>
2024-06-08 22:14:50 -0700

    Update SpotList.py

--- SpotList.py
+++ SpotList.py
@@ -14,7 +14,7 @@ import datetime
 # Load environment variables from .env file
 load_dotenv()
 
-SPOTIFY_CLIENT_ID = "SPOTIFY_CLIENT_ID"
+SPOTIFY_CLIENT_ID = os.getenv('SPOTIFY_CLIENT_ID')
 SPOTIFY_CLIENT_SECRET = os.getenv('SPOTIFY_CLIENT_SECRET')
 REDIRECT_URI = 'http://localhost:5000/callback'
 
